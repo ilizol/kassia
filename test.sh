580acc55664816427dd40211163b0d88ba768e06
Elias Zolotas <ilizol@yahoo.gr>
2020-09-19 11:50:19 +0300

    Add test commands

---
+++ test.sh
@@ -0,0 +1,11 @@
+# examples
+python3 kassia.py examples/kontakion_prophet_elias_1.xml examples/kontakion_prophet_elias_1.pdf
+python3 kassia.py examples/kontakion_prophet_elias_2.xml examples/kontakion_prophet_elias_2.pdf
+python3 kassia.py examples/sample.xml examples/sample.pdf
+python3 kassia.py examples/sample2.xml examples/sample2.pdf
+python3 kassia.py sample3.xml sample3.pdf
+
+# tests
+python3 kassia.py tests/chronos_test.xml tests/chronos_test.pdf
+python3 kassia.py tests/fthora_test.xml tests/fthora_test.pdf
+python3 kassia.py tests/martyria_test.xml tests/martyria_test.pdf
